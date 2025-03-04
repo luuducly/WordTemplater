d3122da315d67eccb64fa9a580cca98296d34260
Lưu Đức Ly <69654714+luuducly@users.noreply.github.com>
2025-03-04 09:22:50 +0700

    Update Program.cs

--- src/WordTemplater.Example/Program.cs
+++ src/WordTemplater.Example/Program.cs
@@ -2,26 +2,34 @@
 using WordTemplater;
 using WordTemplater.Example;
 
-var json1 = File.ReadAllText("DataSamples\\Data1.json");
-var data1 = JObject.Parse(json1);
+var json = File.ReadAllText("DataSamples\\Data.json");
+var data = JObject.Parse(json);
 var equationFile = File.ReadAllBytes("Templates\\Equation.docx");
-data1["Word"] = Convert.ToBase64String(equationFile);
+data["Word"] = Convert.ToBase64String(equationFile);
 
 var avatarFile = File.ReadAllBytes("Templates\\Author.jpg");
-data1["Image"] = Convert.ToBase64String(avatarFile);
+data["Image"] = Convert.ToBase64String(avatarFile);
 
-using (var templateStream = File.OpenRead("Templates\\Template1.docx"))
+var fileName = "Output.docx";
+using (var templateStream = File.OpenRead("Templates\\Template.docx"))
 {
     using (var wordTemplate = new WordTemplate(templateStream))
     {
         wordTemplate.RegisterEvaluator("customizable", new Number2TextEvaluator());
         wordTemplate.RegisterEvaluator("upperFirstLetter", new UpperCaseFirstLetter());
-        using (var exportedStream = wordTemplate.Export(data1))
+        using (var exportedStream = wordTemplate.Export(data))
         {
-            using (var output = File.Create("Output1.docx"))
+            using (var output = File.Create(fileName))
             {
                 exportedStream.CopyTo(output);
             }
         }    
     }
 }    
+
+var p = new Process();
+p.StartInfo = new ProcessStartInfo(fileName)
+{
+   UseShellExecute = true
+};
+p.Start();
